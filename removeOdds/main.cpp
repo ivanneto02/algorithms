ea11e54d181fddfcf816a32bb1e4aafc7770342a
Ivan Neto <72827265+ivanneto02@users.noreply.github.com>
2021-03-16 16:26:29 -0700

    Create main.cpp
    Remove odd numbers from a linked list

---
+++ removeOdds/main.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+
+	int value;
+	Node* next;
+
+	Node() : next(nullptr) {}
+	Node(int value) : next(nullptr), value(value) {}
+
+};
+
+class List {
+
+	public:	
+		Node* head;
+	
+		void removeOdds();
+		void display();
+
+		List() : head(nullptr) {}
+		List(Node* head) : head(head) {}
+
+};
+
+void List::display() {
+
+	for (Node* curr = this->head; curr != nullptr; curr = curr->next) {
+
+		cout << curr->value << " ";
+	}
+
+	cout << endl;
+
+}
+
+void List::removeOdds() {
+
+	if (this->head == nullptr) {
+
+		return;
+	}
+
+	Node* prev = this->head;
+	Node* curr = prev->next;
+
+	while (prev == this->head && prev->value % 2 != 0) {
+
+		Node* tmp = prev;
+		this->head = tmp->next;
+
+		prev = this->head;
+		curr = prev->next;
+
+		delete tmp;
+
+	}
+
+	while (curr != nullptr) {
+
+		if (curr->value % 2 != 0) {
+
+			Node* tmp = curr;
+
+			prev->next = curr->next;
+			curr = prev->next;
+
+			delete tmp;
+	
+			continue;
+		}
+
+		prev = prev->next;
+		curr = prev->next;
+		
+	}
+
+}
+
+int main() {
+	
+	Node* node1 = new Node(1);
+	Node* node2 = new Node(2);
+	Node* node3 = new Node(2);
+	Node* node4 = new Node(5);
+	Node* node5 = new Node(6);
+
+	node1->next = node2;
+	node2->next = node3;
+	node3->next = node4;
+	node4->next = node5;
+
+	List newList(node1);
+
+	newList.removeOdds();
+
+	newList.display();
+
+}
